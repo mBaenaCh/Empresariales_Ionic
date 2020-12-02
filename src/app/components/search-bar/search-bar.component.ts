91da6ad561e4120c07e63d0a11df5c1ecf4a9934
daniel.monroy <daniel.monroym@udea.edu.co>
2020-12-02 18:00:37 -0500

    Added searchbar

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from 'src/app/services/search.service';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+  providers: [Keyboard]
+})
+export class SearchBarComponent implements OnInit {
+  search:any;
+  items: any[] = [];
+  constructor(private searchService:SearchService, private keyboard:Keyboard) { }
+
+  ngOnInit() {}
+
+  generarItems():void{
+    this.searchService.getItems(this.search).subscribe((data) => {
+      console.log(data);
+      this.items = data.results;
+      console.log(this.items);
+
+      this.search="";
+      this.keyboard.hide();
+  });
+
+}
+
+}
---
+++ src/app/services/search.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  baseURL='https://api.mercadolibre.com/sites/MCO/search?q=';
+  URL='https://api.mercadolibre.com/items/';
+  query:any;
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  getItems(query): Observable<any> {
+    console.log(`${this.baseURL}`+`${query}`);
+    return this.http.get(`${this.baseURL}${query}`);
+
+  }
+
+  getItemDetails( query:any): Observable<any> {
+
+    return this.http.get(`${this.URL}${query}`);
+
+  }
+}
